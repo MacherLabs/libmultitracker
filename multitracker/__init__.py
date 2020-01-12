0dce46fb34388a98821aea72f4bf8eee1243ce63
saurabh1993 <saurabh1993.kumar@gmail.com>
2020-01-12 17:56:30 +0530

    making module import python3 compatible

--- multitracker/__init__.py
+++ multitracker/__init__.py
@@ -1,3 +1,8 @@
+import os
+import sys
+abs_path=os.path.abspath(os.path.dirname(__file__))
+sys.path.append(abs_path)
+
 from kalmantracking import KalmanTracker
 from multitracker import MultiTracker
 from singletracker import CorrelationTracker
